d7d6a2f3dc2fc988125d08261837180bc09174b1
Hedgehog333 <artem1100.94@gmail.com>
2021-10-03 08:04:47 +0300

    implementation created tools

--- src/main/java/com/trustly/api/paymentapiintegration/controller/CallApiController.java
+++ src/main/java/com/trustly/api/paymentapiintegration/controller/CallApiController.java
@@ -4,44 +4,51 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
+import com.trustly.api.paymentapiintegration.tools.RestTemplateWithSSL;
 import com.trustly.api.paymentapiintegration.models.Attributes;
 import com.trustly.api.paymentapiintegration.models.Deposit;
 import com.trustly.api.paymentapiintegration.models.TrustlyAPIDepositCredentials;
 import com.trustly.api.paymentapiintegration.models.TrustlyAPIDepositWrapper;
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CallApiController {
 
+    @Autowired
+    TrustlyAPIDepositCredentials trustlyAPIDepositCredentials;
+
+    final String trustlyApiUrl = "https://test.trustly.com/api/1";
+
     @RequestMapping(value = "/call", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public String call(Deposit deposit, Attributes attributes) {
         deposit.setAttributes(attributes);
+        trustlyAPIDepositCredentials.setData(deposit);
 
-        final String trustlyApiUrl = "https://test.trustly.com/api/1";
-
-        HttpClient httpClient = HttpClientBuilder.create().build();
-        ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-        RestTemplate restTemplate = new RestTemplate(requestFactory);
-
-        String result = restTemplate.getForObject(trustlyApiUrl, String.class, makeJsonObject(deposit));
+        String result;
+        try {
+            result = RestTemplateWithSSL
+                    .getRestTemplateWithSSL(trustlyAPIDepositCredentials.getSignature())
+                    .postForObject(
+                            trustlyApiUrl,
+                            makeJsonObject().toString(),
+                            String.class);
+        } catch (Exception e) {
+            result = e.getMessage();
+        }
 
         return result;
     }
 
-    private JsonObject makeJsonObject(Deposit deposit) {
+    private JsonObject makeJsonObject() {
         Gson gson = new Gson();
-        JsonObject wrapper = gson.toJsonTree(new TrustlyAPIDepositWrapper(null)).getAsJsonObject();
+        JsonObject wrapper = gson.toJsonTree(new TrustlyAPIDepositWrapper()).getAsJsonObject();
 
         Gson gsonBuilderUpperCamelCase = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-        JsonObject content = gsonBuilderUpperCamelCase.toJsonTree(new TrustlyAPIDepositCredentials(deposit)).getAsJsonObject();
+        JsonObject content = gsonBuilderUpperCamelCase.toJsonTree(trustlyAPIDepositCredentials).getAsJsonObject();
 
         wrapper.add("params", content);
 
